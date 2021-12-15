2e091a23f15efa33598259052531c20021a6a82d
Mister Balise <74428850+SNEAXIII@users.noreply.github.com>
2021-12-15 07:22:46 +0000

    nothing i guess

--- main.py
+++ main.py
@@ -3,37 +3,50 @@ from jtape_au_sud import *
 ##Parcours DFS
 
 chateau = Graphe()
-chateau.ajouterArete('A','B')
-chateau.ajouterArete('A','D')
-chateau.ajouterArete('D','E')
-chateau.ajouterArete('E','B')
-chateau.ajouterArete('E','C')
-chateau.ajouterArete('C','G')
-chateau.ajouterArete('F','C')
-chateau.ajouterArete('C','E')
+chateau.ajouterArete('A', 'B')
+chateau.ajouterArete('A', 'D')
+chateau.ajouterArete('D', 'E')
+chateau.ajouterArete('E', 'B')
+chateau.ajouterArete('E', 'C')
+chateau.ajouterArete('C', 'G')
+chateau.ajouterArete('F', 'C')
+chateau.ajouterArete('C', 'E')
+chateau.ajouterArete('C', 'F')
 chateau.afficher()
-#Exercice 2
-def parcoursProf1(g,s):
-    assert s in g, "le sommet est pas dans le graphe"
-    rez = set(s)
-    a_visiter = Pile().empiler(s)
-    while a_visiter != None :
-        sommet = a_visiter.depiler()
-        #ps
-
-
-def existeChemin(g,s1,s2):
-
-    pass
 
 #Exercice 3
+
+
+def parcoursProf1(g, s):
+    assert s in g.sommets(), "le sommet est pas dans le graphe"
+    rez = set(s)
+    a_visiter = Pile()
+    a_visiter.empiler(s)
+    while not a_visiter.estVide():
+        sommet = a_visiter.depiler()
+        rez.add(sommet)
+        for Vois in g.voisins(sommet):
+            if not Vois in rez:
+                a_visiter.empiler(Vois)
+    return rez
+
+
+print(parcoursProf1(chateau, "A"))
+
+
+def existeChemin(g, s1, s2):
+    return s2 in parcoursProf1(g, s1)
+
+
+#Exercice 4
 """Programmez la fonction en utilisant les deux lignes de codes présentes"""
 
-def parcoursProfRecurr(g,visites,s):
+
+def parcoursProfRecurr(g, visites, s):
     """réalise le parcours en profondeur d'un graphe g à partir du sommet s et enregistre les sommets visités dans l'ensemble  visites"""
 
     if s not in visites:
-
+        for individus in pla
         pass
 
     for vois in g.voisins(s):
@@ -41,40 +54,36 @@ def parcoursProfRecurr(g,visites,s):
         pass
 
 
-#Exercice 4
+#Exercice 5
 
 
 #version itérative
-def parcours_ch(g,s):
+def parcours_ch(g, s):
 
     pass
+
 
 #version récursive
-def parcours_ch(g,s):
+def parcours_ch(g, s):
 
     pass
 
 
-def creerChemin(g,s1,s2):
+def creerChemin(g, s1, s2):
 
     pass
 
 
-
-#Exercice 5
-
+#Exercice 6
 """Programmez le graphe de l'exercice"""
 
+
 #q4
-def chercheCycle_S(g,marques,s):
+def chercheCycle_S(g, marques, s):
 
     pass
-
 
 
 ##Parcours BFS
-#Exercice 6
+#Exercice 7
 """Programmez la ou les fonctions nécéssaires pour réaliser le parcours en largeur d'un graphe"""
-
-
-    
